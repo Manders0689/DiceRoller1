d8f10fdb7db950f4d0eb5dde331c4fb2fed43a65
Manders0689 <Mandy.beck05@gmail.com>
2022-12-12 21:48:36 -0700

    Finished app with images

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,19 +2,18 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
  * This activity allows a user to roll a dice and view the results on the screen.
  */
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton20: Button = findViewById(R.id.buttonD20)
-        rollButton20.setOnClickListener { rollDice20() }
         val rollButton4: Button = findViewById(R.id.buttonD4)
         rollButton4.setOnClickListener { rollDice4() }
         val rollButton6: Button = findViewById(R.id.buttonD6)
@@ -25,31 +24,26 @@ class MainActivity : AppCompatActivity() {
         rollButton10.setOnClickListener { rollDice10() }
         val rollButton12: Button = findViewById(R.id.buttonD12)
         rollButton12.setOnClickListener { rollDice12() }
+        val rollButton20: Button = findViewById(R.id.buttonD20)
+        rollButton20.setOnClickListener { rollDice20() }
     }
 
     /**
-     * Roll the 20-sided dice and update the screen with the result.
+     * Roll the 6-sided dice and update the screen with the result.
      */
-    private fun rollDice20() {
-        // create dice with 20 sides
-        val dice = Dice(20)
-        // Roll dice and save result
-        val diceRoll = dice.roll()
-        // Find TextView by calling findViewByID()
-        val resultTextView: TextView = findViewById(R.id.textView20)
-        // Convert diceRoll from Int to Text
-        resultTextView.text = diceRoll.toString()
-    }
 
     private fun rollDice4() {
         // create dice with 4 sides
         val dice = Dice(4)
         // Roll dice and save result
         val diceRoll = dice.roll()
-        // Find TextView by calling findViewByID()
-        val resultTextView: TextView = findViewById(R.id.textView4)
-        // Convert diceRoll from Int to Text
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageViewD4)
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.die4_1)
+            2 -> diceImage.setImageResource(R.drawable.die4_2)
+            3 -> diceImage.setImageResource(R.drawable.die4_3)
+            4 -> diceImage.setImageResource(R.drawable.die4_4)
+        }
     }
 
     private fun rollDice6() {
@@ -57,10 +51,15 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         // Roll dice and save result
         val diceRoll = dice.roll()
-        // Find TextView by calling findViewByID()
-        val resultTextView: TextView = findViewById(R.id.textView6)
-        // Convert diceRoll from Int to Text
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageViewD6)
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.die6_1)
+            2 -> diceImage.setImageResource(R.drawable.die6_2)
+            3 -> diceImage.setImageResource(R.drawable.die6_3)
+            4 -> diceImage.setImageResource(R.drawable.die6_4)
+            5 -> diceImage.setImageResource(R.drawable.die6_5)
+            6 -> diceImage.setImageResource(R.drawable.die6_6)
+        }
     }
 
     private fun rollDice8() {
@@ -68,21 +67,37 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(8)
         // Roll dice and save result
         val diceRoll = dice.roll()
-        // Find TextView by calling findViewByID()
-        val resultTextView: TextView = findViewById(R.id.textView8)
-        // Convert diceRoll from Int to Text
-        resultTextView.text = diceRoll.toString()
-}
+        val diceImage: ImageView = findViewById(R.id.imageViewD8)
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.die8_1)
+            2 -> diceImage.setImageResource(R.drawable.die8_2)
+            3 -> diceImage.setImageResource(R.drawable.die8_3)
+            4 -> diceImage.setImageResource(R.drawable.die8_4)
+            5 -> diceImage.setImageResource(R.drawable.die8_5)
+            6 -> diceImage.setImageResource(R.drawable.die8_6)
+            7 -> diceImage.setImageResource(R.drawable.die8_7)
+            8 -> diceImage.setImageResource(R.drawable.die8_8)
+        }
+    }
 
     private fun rollDice10() {
         // create dice with 10 sides
         val dice = Dice(10)
         // Roll dice and save result
         val diceRoll = dice.roll()
-        // Find TextView by calling findViewByID()
-        val resultTextView: TextView = findViewById(R.id.textView10)
-        // Convert diceRoll from Int to Text
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageViewD10)
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.die10_0)
+            2 -> diceImage.setImageResource(R.drawable.die10_1)
+            3 -> diceImage.setImageResource(R.drawable.die10_2)
+            4 -> diceImage.setImageResource(R.drawable.die10_3)
+            5 -> diceImage.setImageResource(R.drawable.die10_4)
+            6 -> diceImage.setImageResource(R.drawable.die10_5)
+            7 -> diceImage.setImageResource(R.drawable.die10_6)
+            8 -> diceImage.setImageResource(R.drawable.die10_7)
+            9 -> diceImage.setImageResource(R.drawable.die10_8)
+            10 -> diceImage.setImageResource(R.drawable.die10_9)
+        }
     }
 
     private fun rollDice12() {
@@ -90,11 +105,53 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(12)
         // Roll dice and save result
         val diceRoll = dice.roll()
-        // Find TextView by calling findViewByID()
-        val resultTextView: TextView = findViewById(R.id.textView12)
-        // Convert diceRoll from Int to Text
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageViewD12)
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.die12_1)
+            2 -> diceImage.setImageResource(R.drawable.die12_2)
+            3 -> diceImage.setImageResource(R.drawable.die12_3)
+            4 -> diceImage.setImageResource(R.drawable.die12_4)
+            5 -> diceImage.setImageResource(R.drawable.die12_5)
+            6 -> diceImage.setImageResource(R.drawable.die12_6)
+            7 -> diceImage.setImageResource(R.drawable.die12_7)
+            8 -> diceImage.setImageResource(R.drawable.die12_8)
+            9 -> diceImage.setImageResource(R.drawable.die12_9)
+            10 -> diceImage.setImageResource(R.drawable.die12_10)
+            11 -> diceImage.setImageResource(R.drawable.die12_11)
+            12 -> diceImage.setImageResource(R.drawable.die12_12)
+        }
     }
+
+    private fun rollDice20() {
+        // create dice with 20 sides
+        val dice = Dice(20)
+        // Roll dice and save result
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageViewD20)
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.die20_1)
+            2 -> diceImage.setImageResource(R.drawable.die20_2)
+            3 -> diceImage.setImageResource(R.drawable.die20_3)
+            4 -> diceImage.setImageResource(R.drawable.die20_4)
+            5 -> diceImage.setImageResource(R.drawable.die20_5)
+            6 -> diceImage.setImageResource(R.drawable.die20_6)
+            7 -> diceImage.setImageResource(R.drawable.die20_7)
+            8 -> diceImage.setImageResource(R.drawable.die20_8)
+            9 -> diceImage.setImageResource(R.drawable.die20_9)
+            10 -> diceImage.setImageResource(R.drawable.die20_10)
+            11 -> diceImage.setImageResource(R.drawable.die20_11)
+            12 -> diceImage.setImageResource(R.drawable.die20_12)
+            13 -> diceImage.setImageResource(R.drawable.die20_13)
+            14 -> diceImage.setImageResource(R.drawable.die20_14)
+            15 -> diceImage.setImageResource(R.drawable.die20_15)
+            16 -> diceImage.setImageResource(R.drawable.die20_16)
+            17 -> diceImage.setImageResource(R.drawable.die20_17)
+            18 -> diceImage.setImageResource(R.drawable.die20_18)
+            19 -> diceImage.setImageResource(R.drawable.die20_19)
+            20 -> diceImage.setImageResource(R.drawable.die20_20)
+        }
+    }
+
 
 class Dice(private val numSides: Int) {
     fun roll(): Int {
